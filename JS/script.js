93bd5cf0ed6a88f1ee841f491dff04302841297e
Wai Mun Liew <wailiew2@gmail.com>
2020-02-13 01:13:34 -0500

    first commit

---
+++ JS/script.js
@@ -0,0 +1,17 @@
+/*
+ * Open the drawer.
+ */
+
+
+
+var menu = document.querySelector('#menu');
+var main = document.querySelector('main');
+var drawer = document.querySelector('.nav');
+
+menu.addEventListener('click', function (e) {
+    drawer.classList.toggle('open');
+    e.stopPropagation();
+});
+main.addEventListener('click', function () {
+    drawer.classList.remove('open');
+}); 
